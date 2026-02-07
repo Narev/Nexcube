6d78a39b750db8f133cf2cbdcaff8b1e55f4564e
Pieter van de Braak <braakpn@gmail.com>
2026-02-08 00:50:36 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+import HomeView from "./views/HomeView.js";
+
+const { createApp, reactive } = Vue;
+
+const state = reactive({view = "home",players = []});
+
+createAPP({
+  components: {
+    HomeView
+  },
+  template: `<HomeView v-if="state.view === 'home'" :state="state" />,
+  setup() {
+    return { state };
+  }
+}).mount("#app");
