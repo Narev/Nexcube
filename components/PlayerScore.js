84aaed3df82e94d34989c4956478a85479387d3f
Pieter van de Braak <braakpn@gmail.com>
2026-02-11 02:56:57 +0100

    test import non global

---
+++ components/PlayerScore.js
@@ -0,0 +1,6 @@
+export default {
+  template: `
+           <div><span style="display:block">Player</span>
+           <span style="display:block">00:14:380</span></div>
+            `
+}
---
+++ views/TimeAttackView.js
@@ -0,0 +1,22 @@
+import PlayerScore from "../components/PlayerScore.js"
+
+export default {
+  template: `
+           <div>
+           <Header></Header>
+           <PlayerScore></PlayerScore>
+           </div>`
+        ,
+      components:{
+        PlayerScore
+      }
+      methods: {
+          setPlayers(amountOfPlayers) {
+            this.state.players = Array.from({length: amountOfPlayers}, () => (
+              {
+                name: "",
+                time: 0,
+              }));
+          }
+      }
+}
