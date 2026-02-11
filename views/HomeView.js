d06840dc0ad36269b897946de9a2ebedbb71776e
Pieter van de Braak <braakpn@gmail.com>
2026-02-11 01:49:42 +0100

    update class for buttons

--- views/HomeView.js
+++ views/HomeView.js
@@ -2,7 +2,7 @@ export default {
   template: `
            <header> <img src="./img/logo.jpg" alt="Company logo" class="logo-full" /> </header>
            <div>
-           <button @click ="setPlayers(1)" class="menu">Time attach</button>
+           <button @click ="setPlayers(1)" class="menu">Time attack</button>
            <button @click ="setPlayers(2)" class="menu">Battle mode</button>
            </div>`
         ,
